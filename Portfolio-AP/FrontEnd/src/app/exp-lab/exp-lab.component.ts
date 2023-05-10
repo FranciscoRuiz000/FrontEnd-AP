c49163c3636552e44dfef0cdd030e83ec76e9e75
FranciscoRuiz000 <121151127+FranciscoRuiz000@users.noreply.github.com>
2023-05-10 01:17:19 -0300

    Backup antes de aplicar backend de experiencia al fronted
    Copia de seguridad, antes de hacer cambios en el fronted, modificando el dashboard y el componente de experiencia laboral para aplicar el backend.

--- Portfolio-AP/FrontEnd/src/app/exp-lab/exp-lab.component.ts
+++ Portfolio-AP/FrontEnd/src/app/exp-lab/exp-lab.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SExperienciaService } from '../Servicios/s-experiencia.service';
+import { Experiencia } from '../model/experiencia';
+import { TokenService } from '../Servicios/token.service';
 
 @Component({
   selector: 'app-exp-lab',
   templateUrl: './exp-lab.component.html',
   styleUrls: ['./exp-lab.component.css']
 })
-export class ExpLabComponent {
+export class ExpLabComponent implements OnInit {
+  expe: Experiencia[] = [];
 
+  constructor(private sExperiencia: SExperienciaService, private tokenService: TokenService) { }
+
+  isLogged = false;
+
+  ngOnInit(): void {
+    this.cargarExperiencia()
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else{
+      this.isLogged = false;
+    }
+  }
+
+  cargarExperiencia():void{
+    this.sExperiencia.lista().subscribe(data=> {this.expe = data;})
+  }
+  
 }
+
